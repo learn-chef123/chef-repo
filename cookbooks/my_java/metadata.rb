dd8b2ce358e6b7862d49b3bff67208e2919c476b
okram999 <nirish.okram@gmail.com>
2016-12-05 16:01:44 -0600

    Adds the java wrapper

--- cookbooks/my_java/metadata.rb
+++ cookbooks/my_java/metadata.rb
@@ -6,6 +6,9 @@ description 'Installs/Configures my_java'
 long_description 'Installs/Configures my_java'
 version '0.1.0'
 
+depends 'java', '~> 1.42.0'
+
+
 # If you upload to Supermarket you should set this so your cookbook
 # gets a `View Issues` link
 # issues_url 'https://github.com/<insert_org_here>/my_java/issues' if respond_to?(:issues_url)
